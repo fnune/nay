71ea2c079c790e1085ea7f5a002b95d0398f3c72
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2019-10-20 13:22:14 +0200

    Move away from tslint in favor of eslint
    With the help of https://github.com/typescript-eslint/tslint-to-eslint-config

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -4,35 +4,33 @@ import React from 'react'
 
 import Rules from '../shared/Rules'
 
-const Popup: React.FC = () => {
-  return (
-    <div
-      style={{
-        width: '300px',
-        maxHeight: '800px',
-        padding: '30px',
-      }}
-    >
-      <div className="container is-fluid">
-        <Rules />
-        <div className="content is-small">
-          <div className="notification is-info">
-            <p>
-              Add your reason for saying Nay! to each rule. That way, you'll remember why they're
-              there in the future.
-            </p>
-            <p>To see why you added a rule, you can hover it.</p>
-          </div>
-          <div className="notification">
-            <p>
-              Rules you add will be used to match against links in all the pages you visit. Those
-              links will then be masked with a random URL and modified so you can't click on them.
-            </p>
-          </div>
+const Popup: React.FC = () => (
+  <div
+    style={{
+      width: '300px',
+      maxHeight: '800px',
+      padding: '30px',
+    }}
+  >
+    <div className="container is-fluid">
+      <Rules />
+      <div className="content is-small">
+        <div className="notification is-info">
+          <p>
+            Add your reason for saying Nay! to each rule. That way, you'll remember why they're
+            there in the future.
+          </p>
+          <p>To see why you added a rule, you can hover it.</p>
+        </div>
+        <div className="notification">
+          <p>
+            Rules you add will be used to match against links in all the pages you visit. Those
+            links will then be masked with a random URL and modified so you can't click on them.
+          </p>
         </div>
       </div>
     </div>
-  )
-}
+  </div>
+)
 
 export default Popup
